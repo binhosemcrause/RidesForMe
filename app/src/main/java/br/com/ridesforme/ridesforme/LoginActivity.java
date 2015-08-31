f65fb10cebf81c6a6b976facbe955d9ffd8bfffb
binhobite <binhobite@hotmail.com>
2015-08-31 01:30:12 -0300

    Retirando classes que não estavam sendo usadas

--- app/src/main/java/br/com/ridesforme/ridesforme/LoginActivity.java
+++ app/src/main/java/br/com/ridesforme/ridesforme/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.okhttp.MultipartBuilder;
@@ -35,9 +37,23 @@ public class LoginActivity extends Activity {
         Button btnLogar = (Button) findViewById(R.id.button2);
         final EditText txtLogin = (EditText) findViewById(R.id.editLogin);
         final EditText txtPassword = (EditText) findViewById(R.id.editPassword);
+        TextView txtCadastrar = (TextView) findViewById(R.id.cadastrarLogin);
+        txtCadastrar.setText(Html.fromHtml("<p><u>Cadastre-se agora!</u></p>"));
         session = new UserSessionManager(getApplication());
 
-        btnLogar.setOnClickListener(new Button.OnClickListener() {
+        btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = txtLogin.getText().toString();
+                String password = txtPassword.getText().toString();
+                session.createUserLoginSession(username, password);
+                Intent intent = new Intent(getApplicationContext(), TesteLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Com WebService
+        /*btnLogar.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String username = txtLogin.getText().toString();
@@ -59,6 +75,14 @@ public class LoginActivity extends Activity {
                 }
 
 
+            }
+        });
+*/
+        txtCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(it);
             }
         });
 
--- app/src/main/java/br/com/ridesforme/ridesforme/MainActivity.java
+++ app/src/main/java/br/com/ridesforme/ridesforme/MainActivity.java
@@ -26,13 +26,47 @@ public class MainActivity extends Activity {
     private boolean splashActive = true;
     private boolean paused = false;
 
-    SharedPreferences pref;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        File f = new File("/data/data/"+getPackageName()+"/shared_prefs/AndroidExamplePref.xml");
 
+        if(f.exists()) {
+            logado();
+        }else {
+            naoLogado();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+    public void logado(){
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    while (splashActive && ms < splashTime) {
+                        if (!paused) {
+                            ms = ms + 100;
+                            Thread.sleep(100);
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(MainActivity.this, TesteLoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    public void naoLogado(){
         Thread timerThread = new Thread() {
             public void run() {
                 try {
@@ -52,12 +86,5 @@ public class MainActivity extends Activity {
         };
         timerThread.start();
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        finish();
-    }
-
 
 }
