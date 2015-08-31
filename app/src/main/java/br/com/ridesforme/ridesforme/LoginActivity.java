281afd7fc110bfbc5695a567091129226b16cd68
binhosemcrause <binhosemcrause@gmail.com>
2015-08-31 17:05:29 -0300

    Começando a usar SharedPreference

--- app/src/main/java/br/com/ridesforme/ridesforme/LoginActivity.java
+++ app/src/main/java/br/com/ridesforme/ridesforme/LoginActivity.java
@@ -1,29 +1,21 @@
 package br.com.ridesforme.ridesforme;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.squareup.okhttp.MultipartBuilder;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
-
-import java.util.concurrent.ExecutionException;
 
 
 public class LoginActivity extends Activity {
@@ -88,4 +80,37 @@ public class LoginActivity extends Activity {
 
     }
 
+    /**
+     * Created by Felipe on 27/08/2015.
+     */
+    public static class LoginControllerTask extends AsyncTask<String, Integer, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            try {
+                OkHttpClient client = new OkHttpClient();
+                RequestBody requestBody = new MultipartBuilder()
+                        .type(MultipartBuilder.FORM) //this is what I say in my POSTman (Chrome plugin)
+                        .addFormDataPart("login",params[0])
+                        .addFormDataPart("senha",params[1])
+                        .build();
+                Request request = new Request.Builder()
+                        .url("http://187.59.115.154:8080/rpg/usuario/login")
+                        .post(requestBody)
+                        .build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    String responseString = response.body().string();
+                    response.body().close();
+                    Log.v("a", responseString);
+                    return Boolean.parseBoolean(responseString);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 }
