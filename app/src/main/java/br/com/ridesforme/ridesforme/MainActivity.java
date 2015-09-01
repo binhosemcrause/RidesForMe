30b4bbc66a9582e581f66ac01c3d2a19aee618c9
binhosemcrause <binhosemcrause@gmail.com>
2015-09-01 13:49:19 -0300

    Mudando o carregamento da tela de Splash

--- app/src/main/java/br/com/ridesforme/ridesforme/MainActivity.java
+++ app/src/main/java/br/com/ridesforme/ridesforme/MainActivity.java
@@ -1,102 +1,35 @@
 package br.com.ridesforme.ridesforme;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.File;
-import java.util.HashMap;
-
-
-public class MainActivity extends Activity {
-    private long ms = 0;
-    private long splashTime = 5000;
-    private boolean splashActive = true;
-    private boolean paused = false;
-
+public class MainActivity extends AppCompatActivity implements Runnable {
+    private boolean logado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*SharedPreferences sf = getSharedPreferences("AndroidExamplePref", MODE_PRIVATE);
-        String chaveSalva = sf.getString("KEY_NAME", "");
-
+        SharedPreferences sf = getSharedPreferences("AndroidExamplePref", MODE_PRIVATE);
+        String chaveSalva = sf.getString("name", "");
         if(chaveSalva != null && !chaveSalva.equals("")){
-            Toast.makeText(getApplicationContext(),chaveSalva,Toast.LENGTH_SHORT).show();
-            logado();
-        }else{
-            naoLogado();
-        }*/
-
-
-        File f = new File("/data/data/"+getPackageName()+"/shared_prefs/AndroidExamplePref.xml");
-        if(f.exists()) {
-            logado();
-        }else {
-            naoLogado();
+            logado = true;
         }
+        Handler handler = new Handler();
+        handler.postDelayed(this, 4000);
     }
-
     @Override
-    protected void onPause() {
-        super.onPause();
+    public void run() {
+        if(!logado){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }else{
+            Intent intent = new Intent(MainActivity.this, TesteLoginActivity.class);
+            startActivity(intent);
+        }
         finish();
     }
-
-    public void logado() {
-        Thread timerThread = new Thread() {
-            public void run() {
-                try {
-                    while (splashActive && ms < splashTime) {
-                        if (!paused) {
-                            ms = ms + 100;
-                            Thread.sleep(100);
-                        }
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    Intent intent = new Intent(MainActivity.this, TesteLoginActivity.class);
-                    startActivity(intent);
-                }
-            }
-        };
-        timerThread.start();
-    }
-
-    public void naoLogado() {
-        Thread timerThread = new Thread() {
-            public void run() {
-                try {
-                    while (splashActive && ms < splashTime) {
-                        if (!paused) {
-                            ms = ms + 100;
-                            Thread.sleep(100);
-                        }
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                }
-            }
-        };
-        timerThread.start();
-    }
-
 }
